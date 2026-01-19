d097f24defefe26485f4547f44a1a5d9ad72a80d
Julia Janse <julia.broman@hotmail.com>
2026-01-19 14:30:00 +0100

    connection and close connection

--- backend/server.js
+++ backend/server.js
@@ -7,26 +7,37 @@ import { WebSocketServer } from 'ws';
 // miljövariabler
 const app = express();
 
-
-
 const PORT = 8080;
+
 // statiska filer
 app.use(express.static('../frontend/public'));
 
 // -----
 
 // skapa en http server, express skickas med som en instans
+const server = http.createServer(app);
+
 
 
 // -----
 
 // skapa en websocket server
-
+const wss = new WebSocketServer({ noServer: true });
 
 // -------
 
 // handshake - godkänn kommunikation via websocket
+server.on("upgrade", (req, socket, head) => {
 
+    console.log("event upgrade...");
+
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        console.log("Client:", req.headers['user-agent']);
+
+
+        wss.emit("connection", ws, req);
+    });
+});
 
 // -----
 
@@ -39,14 +50,28 @@ app.use(express.static('../frontend/public'));
 
 
 // lyssna på events 
+wss.on('connection', (ws) => {
 
+    console.log(`Antal klienter anslutna: ${wss.clients.size}`);
+
+    const obj = { msg: "Ny klient ansluten" };
+
+    ws.send(JSON.stringify(obj));
+
+
+    ws.on('close', () => {
+        console.log(`Klient lämnade, klienter kvar: ${wss.clients.size}`);
+    });
+
+
+
+});
 
 // -----
 
 
-
 // starta server 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Serven lyssnar på port: ${PORT}`);
 });
 
--- frontend/public/scripts/code.js
+++ frontend/public/scripts/code.js
@@ -1,3 +1,5 @@
+import { json } from "express";
+
 // DOM element
 const form = document.querySelector("form");
 const msgElement = document.querySelector("input#msg");
@@ -20,6 +22,11 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     console.log("yes yes yes");
+
+    const msg = msgElement.value;
+    const obj = { msg: msg };
+
+    websocket.send(JSON.stringify(obj));
 });
 
 // aktivera lyssnare på input#msg: kan användas för att visa att ngn skriver "...is typing"
@@ -27,9 +34,15 @@ msgElement.addEventListener("keydown", (e) => {
     console.log("...is typing", e.key);
 
     // hantera att en person skriver ngt - kan kanske skickas som en händelse
-    
+
 });
 
+websocket.addEventListener("message", (e) => {
+    const data = e.data;
 
+
+    const obj = JSON.parse(e.data);
+    console.log("obj", obj);
+});
 
 
